1d18d463d1b813f18d5fdb94bcce916472fdc736
DevHumbleChris <dev.humblechris254@gmail.com>
2023-12-14 16:02:35 +0300

    added Auth component

--- components/global/home/Auth.vue
+++ components/global/home/Auth.vue
@@ -1,5 +1,108 @@
+<script setup>
+import Signin from "~/components/global/home/Signin.vue";
+</script>
+
 <template>
-  <section class="p-10">
-    <div class="">auth</div>
+  <section class="p-10 text-white -mt-[4rem]">
+    <div>
+      <div
+        class="max-w-7xl mx-auto space-y-2 py-16 px-10 sm:px-6 lg:px-8 sm:text-center"
+      >
+        <Icon
+          name="simple-icons:authelia"
+          class="w-12 h-auto border border-white p-2 rounded-md"
+        />
+        <p
+          class="mt-1 text-4xl font-extrabold sm:text-5xl sm:tracking-tight lg:text-6xl"
+        >
+          <span class="text-[#00dc82]"> Auth </span>
+          Components
+        </p>
+        <p class="max-w-3xl mt-5 mx-auto text-xl text-gray-100">
+          An optimized auth component experience thats also completely
+          customizable. Choose the auth strategies you need — passwords, email
+          codes or links, OAuth and more.
+        </p>
+      </div>
+    </div>
+    <div class="grid grid-cols-6 gap-10">
+      <div
+        class="self-start col-span-2 bg-[#040823] rounded-md border border-gray-700"
+      >
+        <div class="flex items-center p-2 gap-3 bg-[#0f172a]">
+          <button
+            class="flex items-center gap-x-1 text-xs font-medium px-2 py-1 rounded-md bg-[#1e293b]"
+          >
+            <Icon name="vscode-icons:file-type-nuxt" class="w-5 h-auto" />
+            <span>Signin.vue</span>
+          </button>
+          <button
+            class="flex items-center gap-x-1 text-xs font-medium px-2 py-1 rounded-md hover:bg-[#1e293b]"
+          >
+            <Icon name="vscode-icons:file-type-nuxt" class="w-5 h-auto" />
+            <span>Signup.vue</span>
+          </button>
+        </div>
+        <div class="p-3 space-y-4">
+          <h2>
+            Let's build your
+            <span class="text-[#00dc82] font-semibold">&lt;SignIn /&gt;</span>
+          </h2>
+          <form action="" class="px-2 space-y-3">
+            <div>
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-white"
+                >Application Name</label
+              >
+              <div class="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  required
+                  class="block w-full rounded-md border-0 px-2 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="My application"
+                />
+              </div>
+            </div>
+            <div>
+              <h2>How will your users signin?</h2>
+              <div>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="hs-basic-usage"
+                    class="relative w-[3.25rem] h-7 p-px bg-gray-100 border-transparent text-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-blue-600 disabled:opacity-50 disabled:pointer-events-none checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600 before:inline-block before:w-6 before:h-6 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-gray-400 dark:checked:before:bg-blue-200"
+                  />
+                  <label for="hs-basic-usage" class="sr-only">switch</label>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="col-span-4">
+        <div class="bg-[#0f172a] rounded-t-md px-4 py-3 flex items-center">
+          <div class="flex items-center gap-x-2">
+            <div class="w-3 h-3 rounded-full bg-[#ed6559]"></div>
+            <div class="w-3 h-3 rounded-full bg-[#ed6559]"></div>
+            <div class="w-3 h-3 rounded-full bg-[#ed6559]"></div>
+          </div>
+          <h1 class="mx-auto">
+            Sign in to
+            <span class="text-[#00dc82] font-semibold"
+              >Christopher Odhiambo</span
+            >
+          </h1>
+        </div>
+        <div
+          class="bg-gradient-to-r from-green-500 to-green-700 rounded-b-md p-10"
+        >
+          <Signin />
+        </div>
+      </div>
+    </div>
   </section>
 </template>
---
+++ components/global/home/Signin.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="bg-white max-w-md mx-auto rounded-3xl p-10 space-y-5">
+    <div class="space-y-4 flex items-center gap-5">
+      <img
+        src="https://tailus.io/sources/blocks/social/preview/images/icon.svg"
+        loading="lazy"
+        class="w-10"
+        alt="tailus logo"
+      />
+      <h2 class="mb-8 text-2xl font-bold text-gray-800 dark:text-white">
+        Sign in to unlock the <br />
+        best of Tailus.
+      </h2>
+    </div>
+    <form action="" class="space-y-3">
+      <div class="space-y-2">
+        <!-- Socials -->
+        <div class="space-y-3">
+          <button
+            class="group w-full relative flex h-11 items-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-white dark:before:bg-gray-700 dark:before:border-gray-600 before:border before:border-gray-200 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 disabled:before:bg-gray-300 disabled:before:scale-100"
+          >
+            <span
+              class="w-full relative flex justify-center items-center gap-3 text-base font-medium text-gray-600 dark:text-gray-200"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="w-5 h-5 absolute left-0"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                />
+              </svg>
+              <span class="text-sm">Continue with Github</span>
+            </span>
+          </button>
+        </div>
+        <!-- End Of Socials -->
+        <div class="flex items-center">
+          <div class="w-full h-[0.05rem] bg-gray-400 rounded-xl"></div>
+          <p class="text-black px-3">Or</p>
+          <div class="w-full h-[0.05rem] bg-gray-400 rounded-xl"></div>
+        </div>
+      </div>
+      <div class="space-y-2">
+        <div>
+          <label
+            for="email"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div>
+          <label
+            for="password"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Password</label
+          >
+          <div class="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="password"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <button
+          type="submit"
+          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
